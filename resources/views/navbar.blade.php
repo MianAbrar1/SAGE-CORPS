285eb4f43ba8f57d92a78a9854c166f4878858f3
MianAbrar1 <abrar.official786@gmail.com>
2024-01-11 14:47:38 +0500

    css

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,25 +1,23 @@
 <a class="logo" href="https://sagecorps.com/"><img width="185" height="56.55"
     src="https://i5de0c.p3cdn1.secureserver.net/wp-content/uploads/2022/05/Sagecorps-logo-e1701131094171.webp?time=1701681697"
     alt="" width="160"></a>
-
-
 <nav class="navbar navbar-expand-lg " id="navbar">
 <ul class="navbar-list">
-    <li class="nav-item dropdown custom-dropdown">
+    <li class="nav-item dropdown custom-dropdown xyz">
         <a class="mobile-logo" href="https://sagecorps.com/"><img width="185" height="56.55"
             src="https://i5de0c.p3cdn1.secureserver.net/wp-content/uploads/2022/05/Sagecorps-logo-e1701131094171.webp?time=1701681697"
             alt="" width="160"></a>
     </li>
-    <li class="nav-item dropdown custom-dropdown">
+    <li class="nav-item dropdown custom-dropdown xyz">
         <a class="navbar-link" href="https://sagecorps.com/how-it-works/program-overview/" id="programsDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            HOW IT WORKS <i class="fa fa-plus" style="font-size:10px"></i>
+            HOW IT WORKS <i class="fa fa-plus nt2" style="font-size:10px"></i>
         </a>
         <div class="dropdown-menu main" aria-labelledby="programsSubDropdown">
             <div class="dropdown sub-main">
                 <a class="dropdown-item"  href="https://sagecorps.com/how-it-works/program-overview/" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false" aria-hidden="true" id="subProgram1Dropdown">
-                    Program Overview <i class="fa fa-plus" style="font-size:10px"></i>
+                    Program Overview <i class="fa fa-plus nt3"></i>
                 </a>
                 <div class="dropdown-menu sub" aria-labelledby="subProgram1Dropdown">
                     <a class="dropdown-item" href="https://sagecorps.com/how-it-works/program-overview/academic-credit/">Academic Credit</a>
@@ -32,10 +30,10 @@
             <div class="dropdown sub-main">
                 <a class="dropdown-item"  href="https://sagecorps.com/how-it-works/finances/" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false" aria-hidden="true" id="subProgram1Dropdown">
-                   Funding your Program <i class="fa fa-plus" style="font-size:10px"></i>
+                   Funding your Program <i class="fa fa-plus nt3"></i>
                 </a>
                 <div class="dropdown-menu sub" aria-labelledby="subProgram1Dropdown">
-                    <a class="dropdown-item" href="https://sagecorps.com/how-it-works/finances/sage-corps-scholarships/diversity-scholarship/">Sage Corps Scholarships <i class="fa fa-plus" style="font-size:10px"></i></a>
+                    <a class="dropdown-item" href="https://sagecorps.com/how-it-works/finances/sage-corps-scholarships/diversity-scholarship/">Sage Corps Scholarships <i class="fa fa-plus nt3"></i></a>
                     <div class="dropdown-menu subdm" aria-labelledby="subProgram1Dropdown" style="margin-left:100%;">
                         <a class="dropdown-item" href="https://sagecorps.com/how-it-works/finances/sage-corps-scholarships/diversity-scholarship/">Diversity Scholarship</a>
                         <a class="dropdown-item" href="https://sagecorps.com/how-it-works/finances/sage-corps-scholarships/entrepreneurship-scholarship/">Entrepreneurship Scholarship</a>
@@ -43,28 +41,28 @@
                     </div>
                 </div>
             </div>
-            <a class="dropdown-item" href="https://sagecorps.com/how-it-works/health-and-safety/">Health andf Safety</a>
+            <a class="dropdown-item" href="https://sagecorps.com/how-it-works/health-and-safety/">Health and Safety</a>
             <a class="dropdown-item" href="https://sagecorps.com/how-it-works/common-questions/">Common Questions</a>
         </div>
     </li>
-    <li class="nav-item dropdown custom-dropdown">
+    <li class="nav-item dropdown custom-dropdown xyz">
         <a class="navbar-link" href="https://sagecorps.com/programs/summer-internship-abroad/" id="programsDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            PROGRAMS <i class="fa fa-plus" style="font-size:10px"></i>
+            PROGRAMS <i class="fa fa-plus nt2" style="font-size:10px"></i>
         </a>
         <div class="dropdown-menu main" aria-labelledby="programsSubDropdown">
             <div class="dropdown sub-main">
                 <a class="dropdown-item"  href="https://sagecorps.com/programs/summer-internship-abroad/" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false" aria-hidden="true" id="subProgram1Dropdown">
-                    Summer Internship Abroad <i class="fa fa-plus" style="font-size:10px"></i>
+                    Summer Internship Abroad <i class="fa fa-plus nt3"></i>
                 </a>
                 <div class="dropdown-menu sub" aria-labelledby="subProgram1Dropdown">
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-amsterdam/">Amsterdam</a>
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-barcelona/">Barcelona</a>
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-berlin/">Berlin</a>
-                    <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-copenhagen/">Copenhagen</a>
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-dublin/">Dublin</a>
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-lisbon/">Lisbon</a>
+                    <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-copenhagen/">Madrid</a>
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-milan/">Milan</a>
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-paris/">Paris</a>
                     <a class="dropdown-item" href="https://sagecorps.com/programs/summer-internship-abroad/intern-in-sydney/">Sydney</a>
@@ -74,7 +72,7 @@
             <div class="dropdown sub-main">
                 <a class="dropdown-item"  href="https://sagecorps.com/programs/study-and-intern-abroad-semester/" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false" aria-hidden="true" id="subProgram1Dropdown">
-                Study and Intern Abroad Semester <i class="fa fa-plus" style="font-size:10px"></i>
+                Study and Intern Abroad Semester <i class="fa fa-plus nt3"></i>
             </a>
             <div class="dropdown-menu sub" aria-labelledby="subProgram1Dropdown">
                 <a class="dropdown-item" href="https://sagecorps.com/programs/study-and-intern-abroad-semester/dublin-semester/">Dublin Semester</a>
@@ -87,17 +85,17 @@
     </li>
 
 
-    <li class="nav-item dropdown custom-dropdown">
+    <li class="nav-item dropdown custom-dropdown xyz">
         <a class="navbar-link" href="https://sagecorps.com/partners/university-partners/" id="programsDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            PARTNERS <i class="fa fa-plus" style="font-size:10px"></i>
+            PARTNERS <i class="fa fa-plus nt2" style="font-size:10px"></i>
         </a>
         <div class="dropdown-menu main" aria-labelledby="programsSubDropdown">
             <a class="dropdown-item" href="https://sagecorps.com/partners/university-partners/">University Partner</a>
             <div class="dropdown sub-main">
                 <a class="dropdown-item"  href="https://sagecorps.com/partners/host-startups/" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false" aria-hidden="true" id="subProgram1Dropdown">
-                    Host Startups <i class="fa fa-plus" style="font-size:10px"></i>
+                    Host Startups <i class="fa fa-plus nt3"></i>
                 </a>
                 <div class="dropdown-menu sub" aria-labelledby="subProgram1Dropdown" >
                     <a class="dropdown-item" href="https://sagecorps.com/partners/host-startups/startup-registration/">Startup Registration</a>
@@ -106,10 +104,10 @@
             </div>
         </div>
     </li>
-    <li class="nav-item dropdown custom-dropdown">
+    <li class="nav-item dropdown custom-dropdown xyz">
         <a class="navbar-link" href="https://sagecorps.com/about-sage-corps/" id="programsDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            ABOUT US <i class="fa fa-plus" style="font-size:10px"></i>
+            ABOUT US <i class="fa fa-plus nt2" style="font-size:10px"></i>
         </a>
         <div class="dropdown-menu main" aria-labelledby="programsSubDropdown">
 
@@ -118,7 +116,7 @@
             <div class="dropdown sub-main">
                 <a class="dropdown-item"  href="https://sagecorps.com/alumni/student-outcomes/" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false" aria-hidden="true" id="subProgram1Dropdown">
-                   Our Alumni <i class="fa fa-plus" style="font-size:10px"></i>
+                   Our Alumni <i class="fa fa-plus nt3"></i>
                 </a>
                 <div class="dropdown-menu sub" aria-labelledby="subProgram1Dropdown">
                     <a class="dropdown-item" href="https://sagecorps.com/alumni/contact-an-alum/">Contact an Alum</a>
@@ -126,14 +124,14 @@
             </div>
         </div>
     </li>
-    <li class="nav-item">
+    <li class="nav-item xyz">
         <a class="navbar-link" href="https://sagecorps.com/blog/">Blog</a>
     </li>
     <li class="nav-item">
-        <a class="navbar-link" href="#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjYzNDkiLCJ0b2dnbGUiOmZhbHNlfQ%3D%3D"><i style="font-size:16px" class="fa nt1">&#xf002;</i></a>
+        <a class="navbar-link nlt" href="#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjYzNDkiLCJ0b2dnbGUiOmZhbHNlfQ%3D%3D"><i style="font-size:16px" class="fa nt1">&#xf002;</i></a>
     </li>
     <li class="nav-item">
-        <a class="btn btn-sm box-button navbar-link" href="https://sagecorps.com/apply/" role="button" data-toggle="dropdown"
+        <a class="btn btn-sm box-button navbar-link nlt" href="https://sagecorps.com/apply/" role="button" data-toggle="dropdown"
         aria-haspopup="true" aria-expanded="false" aria-hidden="true" id="subProgram1Dropdown"><strong> Apply Now </strong></a>
     </li>
 </div>
